5da1c9ed95d61a3604b27b9d453f1fbbba52a4bc
junjgit <junjuntilla@hotmail.com>
2025-02-14 12:33:09 -0500

    initial commit

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/https://github.com/junjgit/plantReact/tree/main",
+  base: "/https://github.com/junjgit/plantReact",
   plugins: [react()],
 })
